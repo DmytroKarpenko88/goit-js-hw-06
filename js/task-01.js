d539c9a4d069d6d76334d14ecf84167386179a86
Dmytro Karpenko <kardmitriy@gmail.com>
2023-02-07 18:37:53 +0200

    task-10

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,20 @@
-// Напиши скрипт, який:
+/* 
+Напиши скрипт, який:
 
-// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
-// В результаті, в консолі будуть виведені наступні повідомлення.
-// Number of categories: 3
+Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+В результаті, в консолі будуть виведені наступні повідомлення.
+Number of categories: 3
 
-// Category: Animals
-// Elements: 4
+Category: Animals
+Elements: 4
 
-// Category: Products
-// Elements: 3
+Category: Products
+Elements: 3
 
-// Category: Technologies
-// Elements: 5
+Category: Technologies
+Elements: 5
+*/
 
 const categoriesEl = document.querySelectorAll('li.item');
 console.log('Number of categories:', categoriesEl.length);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,65 @@
+/* Напиши скрипт створення і очищення колекції елементів. 
+Користувач вводить кількість елементів в input і натискає кнопку Створити, 
+після чого рендериться колекція. 
+Натисненням на кнопку Очистити, 
+колекція елементів очищається.
+
+<div id="controls">
+  <input type="number" min="1" max="100" step="1" />
+  <button type="button" data-create>Create</button>
+  <button type="button" data-destroy>Destroy</button>
+</div>
+
+<div id="boxes"></div>
+
+Створи функцію createBoxes(amount), яка приймає один параметр - число. 
+Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+Розміри найпершого <div> - 30px на 30px.
+Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+Всі елементи повинні мати випадковий колір фону у форматі HEX. 
+Використовуй готову функцію getRandomHexColor для отримання кольору.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+Створи функцію destroyBoxes(), яка очищає вміст div#boxes, 
+у такий спосіб видаляючи всі створені елементи.
+*/
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  boxes: document.querySelector('#boxes'),
+  create: document.querySelector('[data-create]'),
+  destroy: document.querySelector('[data-destroy]'),
+  countBoxes: document.querySelector('input[type="number"]'),
+};
+
+const onCreateBox = refs.create.addEventListener('click', createBoxes);
+const onDestroyBox = refs.destroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  amount = refs.countBoxes.value;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const hexColor = getRandomHexColor();
+
+    box.classList.add('box');
+    box.style.width = `${boxSize + i * 10}px`;
+    box.style.height = `${boxSize + i * 10}px`;
+    box.style.backgroundColor = hexColor;
+    refs.boxes.append(box);
+  }
+}
+
+function destroyBoxes() {
+  const box = document.querySelector('#boxes');
+  box.innerHTML = '';
 }
